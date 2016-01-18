4d91e23dbedae1e3946f1e8f23a3f560ce35f853
KrauseFx <KrauseFx@gmail.com>
2016-01-17 17:51:23 -0800

    Integrated snapshot

---
+++ ThemojiUITests/ThemojiUITests.swift
@@ -0,0 +1,36 @@
+//
+//  ThemojiUITests.swift
+//  ThemojiUITests
+//
+//  Created by Felix Krause on 17/01/16.
+//  Copyright © 2016 Felix Krause. All rights reserved.
+//
+
+import XCTest
+
+class ThemojiUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        continueAfterFailure = false
+
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+    }
+    
+    func testExample() {
+        let app = XCUIApplication()
+        snapshot("01Landing")
+
+        app.collectionViews.staticTexts.elementBoundByIndex(0).tap()
+        snapshot("02Category")
+        app.tables.staticTexts.elementBoundByIndex(0).tap()
+        
+        let searchEmojiTextField = app.textFields["Search Emoji"]
+        searchEmojiTextField.tap()
+        searchEmojiTextField.typeText("air")
+        snapshot("04Filter")
+    }
+}
